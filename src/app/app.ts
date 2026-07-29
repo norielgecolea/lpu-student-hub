8ec68aa5b243414f40d07e2b250741ee1a9b133d
Noriel Gecolea <angelgecolea23@gmail.com>
2026-07-29 16:31:03 +0800

    asd

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,6 +11,7 @@ export class App {
     { name: 'LMS', url: 'https://lms.lpulaguna.edu.ph', description: 'Learning Management System' },
     { name: 'Office 365', url: 'https://office.com', description: 'Email and productivity tools' },
     { name: 'Student Portal', url: 'https://students.lpulaguna.edu.ph/Student/Login.php', description: 'Grades, records, and profile' },
-    { name: 'Reservation', url: 'https://reservation.lpulaguna.com', description: 'Book rooms and facilities' }
+    { name: 'Reservation', url: 'https://reservation.lpulaguna.com', description: 'Book rooms and facilities' },
+    { name: 'Internet', url: 'http://web.lpu-laguna.edu.ph:8090', description: 'LPU Captive Portal' }
   ];
 }
